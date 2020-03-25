2e4c238ead82121f0d082f8f3f11730d5eac96f0
shaz <imshazbangash@gmail.com>
2020-03-25 06:48:56 +0500

    css updates

--- src/component/TaskItem.js
+++ src/component/TaskItem.js
@@ -10,32 +10,37 @@ function TaskItem(props) {
     function handleChange() {
         props.handleUpdate({ data: props.data, index: props.index, time: moment().format('h:mm') })
     }
-    
+
     return (
+
+
         <div className="outerwrap-user">
             <div className="innerwrap-user">
                 <div className="user-img">
-                    <img src={url?url:avatar} alt="" />
+                    <img src={url ? url : avatar} alt="" />
                 </div>
                 <div className="user-text">
                     <h4>{title}</h4>
                     <p>{description}</p>
                 </div>
                 <div className="time">
-                    {!completed ? <input type="checkbox" onChange={() => { handleChange() }} /> : <span>{time}</span>}
+                    {/* {!completed ? <input type="checkbox" onChange={() => { handleChange() }} /> : <span>{time}</span>} */}
                     {/* <span>{time}</span> */}
+                    <div class="form">
+                        <input type="checkbox" class="larger" name="checkBox2" onChange={() => { handleChange() }} />
+                    </div>
                 </div>
             </div>
-            </div>
-   
-  );
+        </div>
+
+    );
 }
 
 const mapDispatchToProps = dispatch => {
     return {
-                handleUpdate: (payload) => dispatch({ type: 'UPDATE', payload:payload}),
+        handleUpdate: (payload) => dispatch({ type: 'UPDATE', payload: payload }),
     }
-  };
+};
 
 
 export default connect(null, mapDispatchToProps)(TaskItem)
